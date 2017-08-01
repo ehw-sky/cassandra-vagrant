475e5d914c04cab36b2aae8554358296c488cfec
Eric Williams <eric.williams@sky.uk>
2017-08-01 13:23:45 +0100

    Initial commit

---
+++ Puppetfile
@@ -0,0 +1,2 @@
+forge 'https://forgeapi.puppetlabs.com'
+mod 'locp-cassandra', '2.5.0'
---
+++ manifests/nodes.pp
@@ -0,0 +1,70 @@
+include cassandra::datastax_repo
+
+node /^cassandra-node[012]$/ {
+  class { 'cassandra':
+    dc             => 'DC1',
+    settings       => {
+      'authenticator'               => 'AllowAllAuthenticator',
+      'auto_bootstrap'              => false,
+      'cluster_name'                => 'MyCassandraCluster',
+      'commitlog_directory'         => '/var/lib/cassandra/commitlog',
+      'commitlog_sync'              => 'periodic',
+      'commitlog_sync_period_in_ms' => 10000,
+      'data_file_directories'       => ['/var/lib/cassandra/data'],
+      'endpoint_snitch'             => 'GossipingPropertyFileSnitch',
+      'hints_directory'             => '/var/lib/cassandra/hints',
+      'listen_interface'            => 'eth0',
+      'num_tokens'                  => 256,
+      'partitioner'                 => 'org.apache.cassandra.dht.Murmur3Partitioner',
+      'saved_caches_directory'      => '/var/lib/cassandra/saved_caches',
+      'seed_provider'               => [
+        {
+          'class_name' => 'org.apache.cassandra.locator.SimpleSeedProvider',
+          'parameters' => [
+            {
+              # 'seeds' => '110.82.155.0,110.82.156.3',
+              'seeds' => 'cassandra-node0.lxc, cassandra-node3.lxc',
+            },
+          ],
+        },
+      ],
+      'start_native_transport'      => true,
+    },
+    service_ensure => 'running',
+  }
+}
+
+node /^cassandra-node[345]$/ {
+  class { 'cassandra':
+    dc             => 'DC2',
+    settings       => {
+      'authenticator'               => 'AllowAllAuthenticator',
+      'auto_bootstrap'              => false,
+      'cluster_name'                => 'MyCassandraCluster',
+      'commitlog_directory'         => '/var/lib/cassandra/commitlog',
+      'commitlog_sync'              => 'periodic',
+      'commitlog_sync_period_in_ms' => 10000,
+      'data_file_directories'       => ['/var/lib/cassandra/data'],
+      'endpoint_snitch'             => 'GossipingPropertyFileSnitch',
+      'hints_directory'             => '/var/lib/cassandra/hints',
+      'listen_interface'            => 'eth0',
+      'num_tokens'                  => 256,
+      'partitioner'                 => 'org.apache.cassandra.dht.Murmur3Partitioner',
+      'saved_caches_directory'      => '/var/lib/cassandra/saved_caches',
+      'seed_provider'               => [
+        {
+          'class_name' => 'org.apache.cassandra.locator.SimpleSeedProvider',
+          'parameters' => [
+            {
+              # 'seeds' => '110.82.155.0,110.82.156.3',
+              'seeds' => 'cassandra-node0.lxc, cassandra-node3.lxc',
+
+            },
+          ],
+        },
+      ],
+      'start_native_transport'      => true,
+    },
+    service_ensure => 'running',
+  }
+}
